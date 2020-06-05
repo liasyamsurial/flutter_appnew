3f63f1a95c7f90f8de7160915e5c521d6ade087a
liasyamsurial <48401025+liasyamsurial@users.noreply.github.com>
2020-06-06 01:02:41 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  GlobalKey<FormState> _key = new GlobalKey();
+  bool _autovalidate = false;
+  String name, profession, message;
+  List<DropdownMenuItem<String>> items = [
+    new DropdownMenuItem(
+        child: new Text('Student'),
+        value: 'Student',
+    ),
+    new DropdownMenuItem(
+        child: new Text('Professor'),
+        value: 'Professor'
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Firebase Database'),
+        ),
+        body: new SingleChildScrollView(
+          child: new Container(
+            padding: new EdgeInsets.all(15.0),
+    child: new Form(
+      key: _key,
+      autovalidate: _autovalidate,
+      child: FormUI(),
+          ),
+        ),
+      ),
+      ),
+    );
+  }
+  Widget FormUI(){
+    return new Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        new Row(
+          children: <Widget>[
+           new Flexible(
+             child: new TextFormField(
+             decoration: new InputDecoration(hintText: 'Name'),
+             validator: validateName,
+             onSaved: (val){
+               name = val;
+             },
+             maxLength: 32,
+           ),
+           ),
+            new SizedBox(width: 10.0),
+            new DropdownButtonHideUnderline(
+                child:  new DropdownButton(
+                  items: items,
+                  hint: new Text('Profession'),
+                  value: profession,
+                  onChanged: (val){
+                    profession= val;
+                  },
+                ))
+          ],
+        ),
+        new TextFormField(
+          decoration: new InputDecoration(hintText: 'Message'),
+          onSaved: (val){
+            message = val;
+          },
+          validator: validateMessage,
+          maxLines: 5,
+          maxLength: 256,
+        ),
+        new RaisedButton(onPressed: _sendToServer,child: new Text('Send'),)
+      ],
+    );
+  }
+  _sendToServer(){
+    if(_key.currentState.validate()) {
+      _key.currentState.save();
+      DatabaseReference ref = FirebaseDatabase.instance.reference();
+      var data = {
+        "name":name,
+        "profession": profession,
+        "message": message,
+      };
+      ref.child('node-name').push().set(data).then((v) {
+        _key.currentState.reset();
+      });
+    }else{
+      setState(() {
+        _autovalidate = true;
+      });
+    }
+  }
+  String validateName(String val){
+    return val.length == 0?"ENTER Name First":null;
+  }
+  String validateMessage(String val){
+    return val.length == 0?"ENTER Message First":null;
+
+  }
+}
